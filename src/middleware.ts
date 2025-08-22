174a6b8877dc2188cbb776249b9deadad70c179b
IndembaEmmanuel <emmanuel.indemba@epitech.eu>
2025-08-22 16:32:57 +0200

    feat : middleware for language and add vercel

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import type { MiddlewareHandler } from 'astro';
+import { languages, defaultLanguage } from './i18n/i18n';
+
+export const onRequest: MiddlewareHandler = async ({ request, locals }, next) => {
+  const url = new URL(request.url);
+  
+  if (url.pathname === '/') {
+    const acceptLanguage = request.headers.get('accept-language') || '';
+    
+    let preferredLanguage = defaultLanguage;
+    
+    const languageTags = acceptLanguage.split(',').map(tag => {
+      const [language] = tag.trim().split(';')[0].split('-');
+      return language.toLowerCase();
+    });
+    
+    for (const lang of languageTags) {
+      if (Object.keys(languages).includes(lang)) {
+        preferredLanguage = lang;
+        break;
+      }
+    }
+    
+    // Rediriger vers la langue préférée
+    return Response.redirect(`${url.origin}/${preferredLanguage}`, 302);
+  }
+  
+  // Continuer normalement pour toutes les autres routes
+  return next();
+};
